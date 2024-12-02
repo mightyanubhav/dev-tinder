1b268a6612ebb51987aedb4cc48da87e303fb219
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-12-02 14:42:03 +0530

    last

--- src/app.js
+++ src/app.js
@@ -3,21 +3,9 @@ const app = express();
 const PORT = 7777;
 
 app.get("/user", (req, res)=>{
+    console.log(req.query);
     res.send({firstName: "anubhav", lastName: "shukla"});
 })
-app.post("/user", (req, res)=>{
-    //savign data to data base
-    res.send("data is saved to the database");
-})
-app.delete("/user", (req, res)=>{
-    //data deleted from database.
-    res.send("data is deleted from the database");
-})
-// when we use app.use it gives response to all http request , to avoid we use different like , get, post etc.
-
-// app.use("/result/test", (req, res)=>{
-//     res.send("result test");
-// })
 
 app.listen(PORT, ()=>{
     console.log(`server up and running on ${PORT}` );
