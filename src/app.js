390872ea543022a6fb26d15da13efb8cf847a139
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-12-03 12:38:53 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 7777;
-const { adminAuth, userAuth } = require("./middlewares/auth");
-// app.use("/admin", adminAuth);
-// app.use("/user", userAuth);
 
-app.get("/admin/getAllData",adminAuth, (req, res)=>{
-    res.send({"firstName" : "Anubhav", "secondName" : "Shukla"})
+app.get("/getUserData", (req, res, next)=>{
+   throw new Error();
 })
-app.get("/admin/deleteAllData", (req, res)=>{
-    res.send("all data deleted");
+app.use("/", (err, req, res, next)=>{
+    if(err){
+        res.send("something went wrong");
+    }else{
+        res.send("all good");
+    }
 })
-
-app.use("/user/allData", userAuth, (req, res) =>{
-    res.send("all user Data");
-})
-
-
 app.listen(PORT, ()=>{
     console.log(`server up and running on ${PORT}` );
 })
